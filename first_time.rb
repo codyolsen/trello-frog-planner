aea852cbe3b32cf427f8c30b4345893615560862
Cody Olsen <codyolsen@users.noreply.github.com>
2016-04-14 14:07:55 -0600

    Adds option to select initial board

--- first_time.rb
+++ first_time.rb
@@ -38,7 +38,7 @@ end
 config = {trello_key: trello_key, trello_token: trello_token}
 
 # Ask what the new board name should be or if they should use an existing board
-puts "Would you like to create a new board or use an existing one?"
+puts "Would you like to create a new board or use an existing one? (No lists will be deleted if you select an exisiting board.)"
 puts "1: New Board"
 puts "2: Existing Board"
 board_option = gets.chomp.to_i
@@ -47,18 +47,28 @@ board_option = gets.chomp.to_i
 if board_option == 1
   puts "New Board Name?"
   board_name = gets.chomp
+  # create new board and set board var
   board = Board.create(name: board_name)
 
 # If existing board display a list of all their boards and give them an integer select
 else
+  boards = Board.all
+  boards.each_with_index do | board, i |
+    puts "#{i}: #{board.name}"
+  end
   # Give/get selection
-  # create new board and set ID
+  puts "Please select the board number."
+  board = boards[gets.chomp.to_i]
 end
 
-list = List.create(name: "Today", board_id: board.id)
 # Using board id create the new lists.
-# Retrieve their ID's in file for generating config hash.
 # Set the proper Date titles for each list.
+list = List.create(name: "Today", board_id: board.id)
+
+
+# Retrieve their ID's in file for generating config hash.
+
+
 
 # Add option to add some sample card data.
 
