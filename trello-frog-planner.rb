964b2538dc961f60851e5e8bdad874d74334f9e9
codyolsen <me@codyolsen.com>
2018-09-11 23:50:12 -0600

    Template work

--- trello-frog-planner.rb
+++ trello-frog-planner.rb
@@ -3,17 +3,29 @@ require_relative 'lib/dates.rb'
 
 # TODO Pull each time segment into it's own file.
 
-def add_templates(source_list, target_list, card_name_template, time_index)
+def add_templates( today, source_list, target_list, card_name_template, due_date, time_index)
   # Trello::CustomFieldItem.find(model.id)
+  
   source_list.cards.each do |card|
     if time_index
       time_indicies = card.custom_field_items.find { |field| field.custom_field.name == "time-indicies" }
-      unless time_indicies.value["text"].split(',').include?(time_index) then return end
+      # If the indicies are not empty, and they don't match the required index, bail out. Don't add this card.
+      if !time_indicies.value["text"].empty? && !time_indicies.value["text"].split(',').include?(time_index) 
+        return
+      end
+    end
+
+    if card.due
+      # new_epoch = today.utc.to_i + (card.due.to_i % 86400)
+      # new_due_date = Time.at(new_epoch)
+      # .iso8601
+      # byebug
     end
 
     Trello::Card.create(
       list_id: target_list.id,
       name: card_name_template % {card_name: card.name},
+      due: new_due_date || "",
       source_card_id: card.id,
       source_card_properties: :all
     )
@@ -59,13 +71,13 @@ if @today == @year.begin
   move_list_cards(@lists[:next_year], @lists[:year])
 
   # Add new reoccuring yearly cards
-  add_templates(@template_lists[:yearly], @lists[:year], "%{card_name} [#{@year.begin.year}]")
+  add_templates( @today, @template_lists[:yearly], @lists[:year], "%{card_name} [#{@year.begin.year}]")
 
   # Update Titles
-  update_titles({
+  update_titles(
     @lists[:next_year]  => "Next Year [#{@next_year.begin.year}]",
     @lists[:year]       => "This Year [#{@year.begin.year}]"
-  })
+  )
 end
 
 # ON THE FIRST OF THE MONTH
@@ -77,7 +89,7 @@ if @today == @month.begin
   process_cards_by_due_date(@lists[:year], {@month.end => @lists[:month]}, @next_month.end => @lists[:next_month] )
 
   # Add new reoccuring monthly cards
-  add_templates(@template_lists[:monthly], @lists[:month], "%{card_name} [#{@month.begin.strftime "%B"}]")
+  add_templates( @today, @template_lists[:monthly], @lists[:month], "%{card_name} [#{@month.begin.strftime "%B"}]")
 
   # Update Titles
   update_titles({
@@ -96,7 +108,7 @@ if @today == @week.begin
   process_cards_by_due_date( @lists[:month], {@week.end => @lists[:week], @next_week.end => @lists[:next_week]} )
 
   # Add new reoccuring weekly cards
-  add_templates(@template_lists[:weekly], @lists[:week], "%{card_name} [#{@week.begin.strftime "%b"} #{@week.begin.day.ordinalize} - #{@week.end.strftime "%b"} #{@week.end.day.ordinalize}]")
+  add_templates( @today, @template_lists[:weekly], @lists[:week], "%{card_name} [#{@week.begin.strftime "%b"} #{@week.begin.day.ordinalize} - #{@week.end.strftime "%b"} #{@week.end.day.ordinalize}]")
 
   # Update Title
   update_titles({
@@ -113,13 +125,13 @@ move_list_cards(@lists[:tomorrow], @lists[:today])
 process_cards_by_due_date(@lists[:week], {@today => @lists[:today], @tomorrow => @lists[:tomorrow]})
 
 # Templates
-add_templates( @template_lists[:daily], @lists[:today], @today.strftime("%{card_name} [%A]"), @today.strftime("%w") )
+add_templates( @today, @template_lists[:daily], @lists[:today], @today.strftime("%{card_name} [%A]"), @today, @today.strftime("%w") )
 
 # Update Title
-update_titles({
+update_titles(
   @lists[:tomorrow] => @tomorrow.strftime("Tomorrow [%A]"),
   @lists[:today]    => @today.strftime("Today [%A]")
-})
+)
 
 
 # Archive Cards from the done list.
